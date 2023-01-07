d60bcc11efb86befd541f04e295650a7c7db464a
mmvergara <mark.jesusmanabat@gmail.com>
2023-01-07 08:37:18 +0800

    fixed api

--- pages/api/discord-bot/interactions.ts
+++ pages/api/discord-bot/interactions.ts
@@ -1,4 +1,10 @@
-import { APIApplicationCommandInteraction, APIInteractionResponse } from "discord-api-types/v10";
+import {
+  APIApplicationCommandInteraction,
+  APIInteraction,
+  APIInteractionResponse,
+  APIPingInteraction,
+  InteractionType,
+} from "discord-api-types/v10";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { rawBodyToString } from "utils/body-parser";
 import { PUBLIC_KEY } from "config";
@@ -6,20 +12,28 @@ import { verifyKey } from "discord-interactions";
 import getCommands from "utils/getCommands";
 import allowedMethod from "utils/error-handling";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<APIInteractionResponse>) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     // Check method
     if (!allowedMethod(req, "POST")) throw new Error("Method not allowed");
+    console.log("INTEREACTIONS");
 
     // Verify discord request
     const signature = req.headers["x-signature-ed25519"] as string | Uint8Array | ArrayBuffer | Buffer;
     const timestamp = req.headers["x-signature-timestamp"] as string | Uint8Array | ArrayBuffer | Buffer;
     const rawBody = await rawBodyToString(req);
     const isValid = verifyKey(rawBody, signature, timestamp, PUBLIC_KEY);
-    if (!isValid) return res.status(400);
+    if (!isValid) return res.status(401).end("invalid request");
 
     // Parse body to get interaction data
-    const interaction = JSON.parse(rawBody) as APIApplicationCommandInteraction;
+    const interactionNew = JSON.parse(rawBody) as APIInteraction;
+
+    // Check if the interaction type is a ping
+    // PING message, respond with ACK (part of Discord's security and authorization protocol)
+    if (interactionNew.type === InteractionType.Ping) return res.status(200).json({ type: 1 });
+
+    // If the interaction type is not a ping it is assumed to be a application command
+    const interaction = interactionNew as APIApplicationCommandInteraction;
 
     // get all commands
     const allCommands = await getCommands();
@@ -30,11 +44,15 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     if (allCommands[commandName]) {
       reply = await allCommands[commandName].execute(interaction);
     }
-    console.log(reply?.type)
+
     if (!reply) throw new Error();
     return res.status(200).json(reply);
-  } catch (error) {
-    return res.status(500).send({ type: 4, data: { content: "Something wen't wrong performing that command" } });
+  } catch (error: any) {
+    console.log(error);
+    console.log("SOMETHING WENT WRONG");
+    return res
+      .status(400)
+      .send({ type: 4, data: { content: "Something wen't wrong performing that command" + error.message } });
   }
 }
 
@@ -43,5 +61,3 @@ export const config = {
     bodyParser: false,
   },
 };
-
-
