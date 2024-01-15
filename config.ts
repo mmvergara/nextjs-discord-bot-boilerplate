22a17e0ae85fbb240c60e40648e21fedc4422c36
mmvergara <mark.jesusmanabat@gmail.com>
2024-01-15 20:32:35 +0800

    updated readme @MooKorea NEXT_PUBLIC_CLIENT_APPLICATION_ID is now renamed to NEXT_PUBLIC_APPLICATION_ID

--- config.ts
+++ config.ts
@@ -1,9 +1,12 @@
-if (!process.env.NEXT_PUBLIC_CLIENT_APPLICATION_ID) throw new Error("APPLICATION_ID is not set");
+if (!process.env.NEXT_PUBLIC_APPLICATION_ID)
+  throw new Error("APPLICATION_ID is not set");
 if (!process.env.BOT_TOKEN) throw new Error("BOT_TOKEN is not set");
 if (!process.env.PUBLIC_KEY) throw new Error("PUBLIC_KEY is not set");
-if (!process.env.REGISTER_COMMANDS_KEY) throw new Error("REGISTER_COMMANDS_KEY is not set");
+if (!process.env.REGISTER_COMMANDS_KEY)
+  throw new Error("REGISTER_COMMANDS_KEY is not set");
 
-export const CLIENT_APPLICATION_ID = process.env.NEXT_PUBLIC_CLIENT_APPLICATION_ID;
+export const CLIENT_APPLICATION_ID =
+  process.env.NEXT_PUBLIC_CLIENT_APPLICATION_ID;
 export const BOT_TOKEN = process.env.BOT_TOKEN;
 export const PUBLIC_KEY = process.env.PUBLIC_KEY;
 export const REGISTER_COMMANDS_KEY = process.env.REGISTER_COMMANDS_KEY;
