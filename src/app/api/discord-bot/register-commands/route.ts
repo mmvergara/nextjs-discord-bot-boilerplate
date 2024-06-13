9ebc1de6897e53b5fbec21cfe2920b861dec8335
mmvergara <mark.jesusmanabat@gmail.com>
2024-06-13 18:42:54 +0800

    Refactor register commands route to be a POST request

--- src/app/api/discord-bot/register-commands/route.ts
+++ src/app/api/discord-bot/register-commands/route.ts
@@ -1,35 +1,30 @@
-import { REGISTER_COMMANDS_KEY } from '@/config';
-import { discord_api } from '@/utils/discord-api';
-import getCommands from '@/utils/getCommands';
+import { REGISTER_COMMANDS_KEY } from "@/config";
+import { discord_api } from "@/utils/discord-api";
+import getCommands from "@/utils/getCommands";
 import { NextResponse } from "next/server";
 
-export async function GET(req: Request) {
-    try {
+export async function POST(req: Request) {
+  try {
+    if (!req.url.endsWith(REGISTER_COMMANDS_KEY))
+      throw new Error("Register commands key was invalid!");
+    const allCommands = await getCommands();
+    const arrayOfSlashCommandsRegister = Object.values(allCommands);
+    const arrayOfSlashCommandsRegisterJSON = arrayOfSlashCommandsRegister.map(
+      (command) => command.register.toJSON()
+    );
 
+    const registerCommands = await discord_api.put(
+      `/applications/${process.env.NEXT_PUBLIC_APPLICATION_ID!}/commands`,
+      arrayOfSlashCommandsRegisterJSON
+    );
 
-        if (!req.url.endsWith(REGISTER_COMMANDS_KEY))
-            throw new Error("Register commands key was invalid!");
-        const allCommands = await getCommands();
-        const arrayOfSlashCommandsRegister = Object.values(allCommands);
-        const arrayOfSlashCommandsRegisterJSON = arrayOfSlashCommandsRegister.map(
-            (command) => command.register.toJSON()
-        );
+    console.log("== COMMANDS REGISTERED ===");
+    console.log(registerCommands.data);
+    console.log("== COMMANDS REGISTERED ===");
 
-        const registerCommands = await discord_api.put(
-            `/applications/${process.env.NEXT_PUBLIC_APPLICATION_ID!}/commands`,
-            arrayOfSlashCommandsRegisterJSON
-        );
-
-        console.log("== COMMANDS REGISTERED ===");
-        console.log(registerCommands.data);
-        console.log("== COMMANDS REGISTERED ===");
-
-        return NextResponse.json({ error: null });
-    } catch (error) {
-        console.error(error);
-
-        return NextResponse.json({
-            error: "Error occured"
-        });
-    }
+    return NextResponse.json({ error: null });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "Error Occured" }, { status: 500 });
+  }
 }
